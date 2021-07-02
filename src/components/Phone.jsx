65f7fda82c675771db9ba2cf2a3914afeb58652a
SebastianC <sebas99cano@gmail.com>
2021-07-02 00:28:15 -0500

    capitulo 3 finalizado

--- src/components/Phone.jsx
+++ src/components/Phone.jsx
@@ -1,18 +1,55 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {DragSource} from 'react-dnd';
+import {ItemTypes} from './Constants';
+
+// phone DnD spec
+const phoneSpec = {
+    beginDrag(props){
+        console.log("begin drag")
+        return{
+            name: props.brand
+
+        }
+    },
+    endDrag(props, monitor, component){
+        if (monitor.didDrop()){
+            const dragItem = monitor.getItem(); // from beginDrag
+            const dropResult = monitor.getDropResult();
+            // Move action goes here
+            console.log("You dropped ", dragItem.name, ' into '+ dropResult.name)
+        }else{
+            return;
+        }
+    }
+}
+
+// phone DragSource collect
+let collect = ( connect, monitor ) =>{
+    return{
+        connectDragSource: connect.dragSource(),
+        isDragging: monitor.isDragging()
+    }
+}
 class Phone extends Component{
     render(){
-        const { name } = this.props;
-        return(
-            <div className="ui card phone">
-                <div className="image"><img src="/images/phone.jpg" />
-                </div>
+        const { brand } = this.props;
+        const { isDragging, connectDragSource } = this.props;
+
+        const opacity = isDragging? 0.4: 1;
+        const style={
+            opacity: opacity
+        };
+        const phoneClass = isDragging? 'ui card phone drag': 'ui card phone';
+        return connectDragSource(
+            <div className={phoneClass} style={style}>
+                <div className="image"><img src="/images/phone.jpg" /></div>
                 <div className="content">
-                    <div className="phone-name">{ name }</div>
+                    <div className="phone-name">{ brand }</div>
                     <div className="meta">8G RAM, 16G memory</div>
                 </div>
                 <div className="extra content">
-                    <a href={"http://localhost:3000/"}>
-                        <i aria-hidden="true" className="money icon" />
+                    <a>
+                        <i aria-hidden="true" className="money icon"></i>
                         $ 80
                     </a>
                 </div>
@@ -21,4 +58,4 @@ class Phone extends Component{
     }
 }
 
-export default Phone
+export default DragSource(ItemTypes.PHONE, phoneSpec, collect)(Phone);
--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
 import Phone from './Phone'
 import ShoppingCart from './ShoppingCart'
 import MyCart from './MyCart'
@@ -7,33 +10,32 @@ class Container extends Component {
     render(){
         const { phones } = this.props
         return(
-            <div className="ui container">
+            <div class="ui container">
                 <div className="main-header">
-                    <h2 className="ui icon center aligned header">
-                        <i aria-hidden="true" className="mobile circular icon"/>
-                        <div className="content title">Phone Shope</div>
+                    <h2 class="ui icon center aligned header">
+                        <i aria-hidden="true" class="mobile circular icon"></i>
+                        <div class="content title">Phone Shope</div>
                     </h2>
                 </div>
-                <div className="ui grid">
+                <div class="ui grid">
                     <h1 className="mycart-header">My cart</h1>
-                    <div className="left floated five wide column my-cart">
+                    <div class="left floated five wide column my-cart">
                         <MyCart />
                     </div>
-                    <div className="right floated five wide column">
-                        <div className="ui row">
-                            <h3 className="brand-heading">Phone Brands</h3>
-                        </div>
-                        <div className="ui row">
-                            <div className="shopping-list">
-                                {Object.keys(phones).map((phone) =>(
-                                    <Phone key={phone} name={phones[phone].brand} />
-                                ))}
+                    <div class="right floated five wide column">
+                        <DndProvider backend={ HTML5Backend } >
+                            <div class="ui row">
+                                <h3 class="brand-heading">Phone Brands</h3>
                             </div>
-                            <div className="shopping-cart">
+                            <div class="ui row">
+                                <div className="shopping-list">
+                                    {Object.keys(phones).map((phone) =>(
+                                        <Phone key={phone} brand={phones[phone].brand} />
+                                    ))}
+                                </div>
                                 <ShoppingCart />
                             </div>
-
-                        </div>
+                        </DndProvider>
                     </div>
                 </div>
             </div>
